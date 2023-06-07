909253ae15e23dd3f9403b94dd2c6f9a04c0979a
Andréia Ferreira <andreiaferreiradev@gmail.com>
2023-06-06 23:16:21 -0300

    fix: validating new ideas form values

--- server.js
+++ server.js
@@ -3,51 +3,6 @@ const server = express()
 
 const db = require("./db")
 
-/* const ideas = [
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/2729/2729007.png",
-        title: "Cursos de Programação",
-        category: "Estudo",
-        description: "O mundo está se transformando, então transforme a sua forma de programar o mundo.",
-        url: "https://www.rocketseat.com.br"
-    },
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/2729/2729005.png",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "A vida é um movimento. Movimente seu corpo para melhorar a sua mente.",
-        url: "https://www.rocketseat.com.br"
-    },
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/2729/2729027.png",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Assim como o corpo a mente busca descanso, meditar ajuda a elevar o seu autoconhecimento.",
-        url: "https://www.rocketseat.com.br"
-    },
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/2729/2729032.png",
-        title: "Karaokê",
-        category: "Diversão em Família",
-        description: "A música transforma a nossa vida, nos faz sonhar, como se estivessemos vivendo uma aventura. Cante!",
-        url: "https://www.rocketseat.com.br"
-    },
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/2729/2729038.png",
-        title: "Pintura",
-        category: "Criatividade",
-        description: "A arte de colorir a vida do seu jeito.",
-        url: "https://www.rocketseat.com.br"
-    },
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/2729/2729048.png",
-        title: "Recortes",
-        category: "Criatividade",
-        description: "Sempre busque ideias que liberem a sua criatividade.",
-        url: "https://www.rocketseat.com.br"
-    },
-] */
-
 server.use(express.static("public"))
 
 server.use(express.urlencoded({ extended: true }))
@@ -94,7 +49,31 @@ server.get("/ideias", function(req, res) {
 })
 
 server.post("/", function(req, res) {
-    console.log(req.body)
+    const query = `
+    INSERT INTO ideas(
+        image,
+        title,
+        category,
+        description,
+        link
+    ) VALUES (?,?,?,?,?);
+`
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        return res.redirect("/ideias")
+    })
 })
 
 server.listen(3000)
